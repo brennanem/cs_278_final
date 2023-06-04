21555761819e7264253319490b99b60e08ef51b2
Kathryn <kathryn.radziwonski@gmail.com>
2023-06-03 22:11:45 -0700

    DMs and new group FE

--- mooch/Screens/DMpage.js
+++ mooch/Screens/DMpage.js
@@ -4,16 +4,36 @@ import * as React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import Messages from './Messages';
+import { db } from "../firebaseConfig";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 function DMpage({ navigation }) {
+
+    // adding backend functionality 
+    const messagesRef = db.collection('messages');
+    const query = messagesRef.orderBy('createdAt').limit(25);
+
+    // listen to data using react hook
+    const [messages] = useCollectionData(query, {idfield: 'id'});
+
     const Drawer = createDrawerNavigator();
     return(
         < KeyboardAvoidingView 
         behavior="padding"
         style={{flex:1}}>
-        <Messages />
+        
+            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
+
         </KeyboardAvoidingView>
     )
+}
+
+function ChatMessage(props) {
+    const { text, uid } = props.message;
+
+    //const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recieved';
+
+    return <p>{text}</p>
 }
 
 export default DMpage;
